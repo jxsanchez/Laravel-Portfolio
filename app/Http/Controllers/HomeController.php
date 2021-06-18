81e89872bfca1197854b6b1145fb3e9bb9640f44
Jesus Sanchez <jxasanchez@gmail.com>
2021-06-18 07:30:29 -0700

    fix anime.js not showing up

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -22,7 +22,7 @@ class HomeController extends Controller
             ],
             (object) [
                 "category" => "Libraries",
-                "skills" => array("jQuery", "Bootstrap", "Anime.js", "Express.js", "Mongoose")
+                "skills" => array("jQuery", "Bootstrap", "anime.js", "Express.js", "Mongoose")
             ],
             (object)[
                 "category" => "Skills",
